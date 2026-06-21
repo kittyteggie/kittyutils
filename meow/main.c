709f5fa45afde65d87a4509d80ab813bfec25c81
Kitty <4lessandro@proton.me>
2026-06-21 22:24:13 +0000

    Create main.c

---
+++ meow/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    if(argc > 1 &&
+        (strcmp(argv[1], "--version") == 0 ||
+        strcmp(argv[1], "-v") == 0)){
+        printf("meow (Kittyutils) v0.01\nRepo: https://github.com/kittyteggie/kittyutils\n");
+        return 0;
+    }
+    for (int i = 1; i < argc; i++){
+        printf("%s", argv[i]);
+
+        if (i < argc - 1){
+            printf(" ");
+        }
+    }
+    printf("\n");
+    return 0;
+}
