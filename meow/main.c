bb0b06e5a3ba9ae0502277cf6ec78e2445946949
Kitty <4lessandro@proton.me>
2026-06-21 22:35:50 +0000

    meow v0.02
    Always remember to betatest your code :/

--- meow/main.c
+++ meow/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]){
-    if(argc > 1 &&
+int main(int argc, char *argv[]) {
+    if (argc > 1 &&
         (strcmp(argv[1], "--version") == 0 ||
-        strcmp(argv[1], "-v") == 0)){
-        printf("meow (Kittyutils) v0.01\nRepo: https://github.com/kittyteggie/kittyutils\n");
+         strcmp(argv[1], "-v") == 0)) {
+        printf("meow (Kittyutils) v0.02\nRepo: https://github.com/kittyteggie/kittyutils\n");
         return 0;
     }
-    for (int i = 1; i < argc; i++){
+    for (int i = 1; i < argc; i++) {
         printf("%s", argv[i]);
-
-        if (i < argc - 1){
+        if (i < argc - 1) {
             printf(" ");
         }
     }
